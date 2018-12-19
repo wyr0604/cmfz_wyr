99b57ae7b807943e62faecb85fe5210e30703ea2
wyr <1140507029@qq.com>
2018-12-19 18:03:24 +0800

    Initial commit

---
+++ src/main/java/com/baizhi/wyr/cmfz_wyr/CmfzWyrApplication.java
@@ -0,0 +1,14 @@
+package com.baizhi.wyr.cmfz_wyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmfzWyrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzWyrApplication.class, args);
+    }
+
+}
+
